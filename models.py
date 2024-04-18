4d3791d684620fbdee272a5f13c8ddd783197319
muskanmandhan <72559642+muskanmandhan@users.noreply.github.com>
2024-04-19 01:26:52 +0530

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Image(models.Model):
-    name = models.CharField(max_length=100)
-    confluence_version = models.CharField(max_length=20)
-    golden_image_version = models.CharField(max_length=20)
